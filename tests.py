388fa47de73b1e680ecb5726f3a3c47311028d61
koniushkovanatalia <koniushkovanatalia@gmail.com>
2025-06-23 13:24:37 +0200

    update tests.py

--- tests.py
+++ tests.py
@@ -3,27 +3,6 @@ import pytest
 from main import BooksCollector
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
-#class TestBooksCollector:
-
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    #def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        #collector = BooksCollector()
-
-        # добавляем две книги
-        #collector.add_new_book('Гордость и предубеждение и зомби')
-        #collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        #assert len(collector.get_books_rating()) == 2
-
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
 
 class TestBooksCollector:
 
@@ -77,18 +56,6 @@ class TestBooksCollector:
 
     def test_get_book_genre_if_book_not_in_books_genre_negative_case(self, collector):
         assert collector.get_book_genre('Я, робот') is None
-
-
-    def test_get_books_with_specific_genre_if_books_in_list_positive_case(self, collector):
-        collector.add_new_book('Я, робот')
-        collector.set_book_genre('Я, робот', 'Фантастика')
-
-        collector.add_new_book('Дюна')
-        collector.set_book_genre('Дюна', 'Фантастика')
-
-        collector.add_new_book('Солярис')
-        collector.set_book_genre('Солярис', 'Фантастика')
-        assert len(collector.get_books_with_specific_genre('Фантастика')) == 3
 
     def test_get_books_with_specific_genre_if_books_have_different_genres_positive_case(self, collector):
         collector.add_new_book('Я, робот')
